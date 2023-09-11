6f0986a1ad4b558337974166416f775027e8a1b6
PrincePandeycode <pandeyprince4723@gmail.com>
2023-09-11 20:42:48 +0530

    unit Tests added

--- test/test_car.py
+++ test/test_car.py
@@ -1,11 +1,44 @@
-import unittest
-from datetime import datetime
 
 from engine.model.calliope import Calliope
 from engine.model.glissade import Glissade
 from engine.model.palindrome import Palindrome
 from engine.model.rorschach import Rorschach
 from engine.model.thovex import Thovex
+import unittest
+from datetime import datetime
+
+import unittest
+from datetime import datetime
+from engine.capulet_engine import CapuletEngine
+from engine.spindler_battery import SpindlerBattery
+
+class TestCapuletEngine(unittest.TestCase):
+    def test_needs_service(self):
+        # Create a CapuletEngine instance with last service date and mileage
+        engine = CapuletEngine(last_service_date=datetime(2022, 1, 1), current_mileage=25000, last_service_mileage=20000)
+        
+        # Test if the engine needs service (should return True)
+        self.assertTrue(engine.needs_service())
+
+        # Test if the engine does not need service (should return False)
+        engine = CapuletEngine(last_service_date=datetime(2022, 1, 1), current_mileage=22000, last_service_mileage=20000)
+        self.assertFalse(engine.needs_service())
+
+class TestSpindlerBattery(unittest.TestCase):
+    def test_needs_service(self):
+        # Create a SpindlerBattery instance with last service date
+        battery = SpindlerBattery(last_service_date=datetime(2022, 1, 1))
+        
+        # Test if the battery needs service (should return True)
+        self.assertTrue(battery.needs_service())
+
+        # Test if the battery does not need service (should return False)
+        battery = SpindlerBattery(last_service_date=datetime(2023, 1, 1))
+        self.assertFalse(battery.needs_service())
+
+if __name__ == '__main__':
+    unittest.main()
+
 
 
 class TestCalliope(unittest.TestCase):
